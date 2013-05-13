c588ab9ca47a2a21e9fc6fa3a3cde88f0ed901d8
NixonFernando <ngilacho.98@gmail.com>
2013-05-13 14:11:52 -0500

    Create quetegusta.py
    n

---
+++ quetegusta.py
@@ -0,0 +1,3 @@
+comida = raw_input ("¿Que te gusta comer?")
+dia = raw_input ("¿Que dia te gusta comerlo?")
+print "ah ahora se que preparar el %s, muy bien voy a comprar los ingredientes para preparar %s" % (dia, comida)
